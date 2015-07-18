7ed273432b3fc57a4363b378bb6a9a93b106a055
Kostast23 <kostas.tri@hotmail.com>
2015-07-18 18:30:30 +0300

    Fixed urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^/', include('webapp.urls', namespace='webapp')),
+    url(r'^', include('webapp.urls', namespace='webapp')),
 ]
