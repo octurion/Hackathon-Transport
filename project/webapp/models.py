292e363d3e13404c3d6e3f7ad84329bd31c1a77c
Kostast23 <kostas.tri@hotmail.com>
2015-07-18 15:24:32 +0300

    Created first model

--- project/webapp/models.py
+++ project/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=128)
+
+
+class Route(models.Model):
+    user = models.ForeignKey(User)
+
+
+class GeoPoint(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    timestamp = models.DateTimeField()
+    id = models.IntegerField()
+    route = models.ForeignKey(Route)
